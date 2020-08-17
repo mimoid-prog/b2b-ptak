fc129bb9a8ed066ed60d479b674e3ca89aad31e0
mimoid-prog <mimoid.prog@gmail.com>
2020-08-17 22:30:19 +0200

    Added gatsby material ui plugin to gatsby config

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -18,6 +18,7 @@ module.exports = {
     `gatsby-plugin-layout`,
     `gatsby-transformer-json`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-material-ui`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
